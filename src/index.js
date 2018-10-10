2091ba9418fca8684694f17dd878aaa7b9b1e48c
Sorin Muchi <smuchi@vigilantglobal.com>
2018-10-10 15:39:33 -0400

    Add env-cmd and transform-inline-environment-variables to share environment variables accross the stack.

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ const resultB = subtract(5, 1);
 console.log(resultA, resultB);
 console.log(styles.localClass); // _19OBmKu4X8SmIISJiYXz8U
 console.log(styles.globalClass); // undefined
+console.log(process.env.APP_NAME);
--- webpack/config.babel.js
+++ webpack/config.babel.js
@@ -24,7 +24,12 @@ export default {
       },
       {
         test: /\.js?$/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['transform-inline-environment-variables'],
+          },
+        },
         exclude: /node_modules/,
       },
       {
